930bef3ed8e842465636fe4dbaa8e7669e2a9a5b
Allex <inta.dn@yandex.ru>
2023-04-17 15:50:52 +0300

    Получение курса валюты

--- public/homePage.js
+++ public/homePage.js
@@ -15,8 +15,22 @@ logoutButton.action = () => {
 ApiConnector.current((responseBody) => {
 	if (responseBody.success) {
 		ProfileWidget.showProfile(responseBody.data);
-	} else {
-		console.error("Произошла ошибка при получении данных пользователя");
 	}
 });
+
+
+// Получение текущих курсов валюты
+const ratesBoard = new RatesBoard();
+
+// Функция для выполнения запроса получения курсов валют
+function getCurrencyRates() {
+  ApiConnector.getStocks((data) => {
+    ratesBoard.clearTable(); // очищаем таблицу
+    ratesBoard.fillTable(data); // заполняем таблицу полученными данными
+  });
+}
+
+getCurrencyRates(); // Вызываем функцию для получения текущих валют
+
+setInterval(getCurrencyRates, 60000); // Устанавливаем интервал для выполнения функции раз в минуту
 
